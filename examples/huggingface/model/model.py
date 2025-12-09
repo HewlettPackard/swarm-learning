ac8f63118ee7af5cff143bf24e715791c694e963
NayantaraK <nayankkur@gmail.com>
2025-12-09 19:44:47 +0530

    Review comments fixed

--- examples/huggingface/model/model.py
+++ examples/huggingface/model/model.py
@@ -1,5 +1,5 @@
 ############################################################################
-## (C)Copyright 2021-2023 Hewlett Packard Enterprise Development LP
+## (C)Copyright 2025 Hewlett Packard Enterprise Development LP
 ## Licensed under the Apache License, Version 2.0 (the "License"); you may
 ## not use this file except in compliance with the License. You may obtain
 ## a copy of the License at
